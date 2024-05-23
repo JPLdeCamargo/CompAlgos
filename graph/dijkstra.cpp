027d4897fb82aa2ee4e56cc697b3d23f31d7efbb
JPLdeCamargo <jplopescamargo@gmail.com>
2024-05-23 17:51:40 -0300

    updating dijkstra

--- graph/dijkstra.cpp
+++ graph/dijkstra.cpp
@@ -3,23 +3,20 @@ using namespace std;
 using ii = pair<int, int>;
 
 vector<int> dijkstra(vector<vector<ii>>& graph, int start){
-    vector<int> dist(graph.size(), INT_MAX);
+    vector<int> dist(graph.size(), INT_MAX/10);
     dist[start] = 0;
-    priority_queue<ii> pq;
+    priority_queue<ii, vector<ii>, greater<ii>> pq;
     pq.push({0, start});
     while(!pq.empty()){
         auto t = pq.top(); pq.pop();
         int d = t.first, u = t.second;
-        if(d == dist[u]){
-            for(auto e:graph[u]){
-                int v = e.second, weight = e.first;
-                if(dist[v] > dist[u] + weight){
-                    dist[v] = dist[u] + weight;
-                    pq.push({dist[v], v});
-                }
+        if (d > dist[u]) continue;
+        for(auto v:graph[u]){
+            if(dist[u] + v.first < dist[v.second]){
+                dist[v.second] = dist[u] + v.first;
+                pq.push({dist[v.second], v.second});
             }
         }
     }
     return dist;
-
 }
